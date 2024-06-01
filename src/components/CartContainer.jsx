d7208baf7634acc91153cf13baf6daa36ddd918f
Qui Nguyen <qui@gmail.com>
2024-06-01 10:34:07 -0400

    first commit

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,12 +1,14 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import CartItem from "./CartItem";
+import { clearCart } from "../features/cart/cartSlice";
 
 const CartContainer = () => {
     const { cartItems, total, amount } = useSelector((state) => state.cart)
+    const dispatch = useDispatch()
+
     if(amount < 1) {
         return <section className="cart">
             <header>
-                <h2>your cart</h2>
                 <h4 className="empty-cart">is currently empty</h4>
             </header>
         </section>
@@ -24,7 +26,7 @@ const CartContainer = () => {
             <div className="cart-total">
                 <h4>total <span>{total}</span></h4>
             </div>
-            <button className="btn clear-btn">clear cart</button>
+            <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>clear cart</button>
         </footer>
     </div>;
 };
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -11,12 +11,16 @@ const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers:{
-
+    clearCart(state){
+        state.cartItems = []
+    }
     }
 
 })
 
 // console.log(cartSlice);
+
+export const {clearCart} = cartSlice.actions
 
 export default cartSlice.reducer
 
