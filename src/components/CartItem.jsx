fa01266001fb8906c18b92f141924e684b8618bc
Qui Nguyen <qui@gmail.com>
2024-06-02 12:34:53 -0400

    first commit

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { ChevronDown, ChevronUp } from '../icons'
-import { removeItem } from '../features/cart/cartSlice';
+import { removeItem, increase, decrease } from '../features/cart/cartSlice';
 const CartItem = ({ id, img, title, price, amount }) => {
     const dispatch = useDispatch()
 
@@ -12,9 +12,15 @@ const CartItem = ({ id, img, title, price, amount }) => {
             <button className="remove-btn" onClick={() => dispatch(removeItem(id))}>remove</button>
         </div>
         <div>
-            <button className="amount-btn"> <ChevronUp /></button>
+            <button className="amount-btn" onClick={() => dispatch(increase({ id }))}> <ChevronUp /></button>
             <p className="amount">{amount}</p>
-            <button className="amount-btn"><ChevronDown /></button>
+            <button className="amount-btn" onClick={() => {
+                if(amount === 1) {
+                    dispatch(removeItem(id))
+                    return
+                }
+                dispatch(decrease({ id }))
+            }}><ChevronDown /></button>
         </div>
     </article>;
 };
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -18,6 +18,15 @@ const cartSlice = createSlice({
         // console.log(action);
         const itemId = action.payload;
         state.cartItems = state.cartItems.filter(item => item.id !== itemId)       
+    },
+    increase(state, {payload}){
+        // console.log(payload);
+        const cartItem = state.cartItems.find(item => item.id === payload.id)
+        cartItem.amount = cartItem.amount + 1;
+    },
+    decrease(state, {payload}){
+        const cartItem = state.cartItems.find(item => item.id === payload.id)
+        cartItem.amount = cartItem.amount - 1;
     }
     }
 
@@ -25,7 +34,7 @@ const cartSlice = createSlice({
 
 // console.log(cartSlice);
 
-export const {clearCart, removeItem} = cartSlice.actions
+export const {clearCart, removeItem, increase, decrease} = cartSlice.actions
 
 export default cartSlice.reducer
 
