92eeb927e2e9219565c4dfaee4a90d1751d7a367
Qui Nguyen <qui@gmail.com>
2024-06-03 23:40:45 -0400

    first commit

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import CartItem from "./CartItem";
-import { clearCart } from "../features/cart/cartSlice";
+// import { clearCart } from "../features/cart/cartSlice";
+import { openModal } from "../features/modal/modalSlice";
 
 const CartContainer = () => {
     const { cartItems, total, amount } = useSelector((state) => state.cart)
@@ -9,6 +10,7 @@ const CartContainer = () => {
     if(amount < 1) {
         return <section className="cart">
             <header>
+                <h2>your cart</h2>
                 <h4 className="empty-cart">is currently empty</h4>
             </header>
         </section>
@@ -26,7 +28,7 @@ const CartContainer = () => {
             <div className="cart-total">
                 <h4>total <span>{total.toFixed(2)}</span></h4>
             </div>
-            <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>clear cart</button>
+            <button className="btn clear-btn" onClick={() => dispatch(openModal())}>clear cart</button>
         </footer>
     </div>;
 };
